39bd5f7b4d530bc1e7a59a49432f7ce5264f8748
yan8909 <43824130+yan8909@users.noreply.github.com>
2020-11-25 17:41:08 +0800

    added merge two sorted list and best time to buy and sell stock

---
+++ 0021.Merge_Two_Sorted_Lists/merge_2_sorted_lists.py
@@ -0,0 +1,33 @@
+# Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+# Example 1:
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+# Example 2:
+# Input: l1 = [], l2 = []
+# Output: []
+
+# Example 3:
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ans = dummy = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                dummy.next = l1
+                l1 = l1.next
+            else:
+                dummy.next = l2
+                l2 = l2.next
+            dummy = dummy.next
+        dummy.next = l1 or l2
+
+        return ans.next
